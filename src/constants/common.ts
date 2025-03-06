b4c28f1400b07e962fc4cd7647a155118bccb2bb
Kellar <kellar.base09@gmail.com>
2025-03-06 13:41:52 +0000

    Retrieve paymaster address

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -90,4 +90,28 @@ export const DecentPaymasterFactoryV1Abi = [
     stateMutability: 'nonpayable',
     type: 'function',
   },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'salt',
+        type: 'uint256',
+      },
+    ],
+    name: 'getAddress',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
 ] as const;
