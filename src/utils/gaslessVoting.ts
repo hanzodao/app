a0ee265579af631f16bdfd89c2319f3a7efa0bff
Kellar <kellar.base09@gmail.com>
2025-03-26 09:38:52 +0000

    remove leftover refs to create smart wallet

--- src/utils/gaslessVoting.ts
+++ src/utils/gaslessVoting.ts
@@ -1,15 +1,6 @@
 import { Address, getContract, keccak256, PublicClient, stringToHex } from 'viem';
 import { DecentPaymasterFactoryV1Abi } from '../assets/abi/DecentPaymasterFactoryV1Abi';
 
-export const getUserSmartWalletSalt = (args: { EOA: Address; chainId: number }) => {
-  const { EOA, chainId } = args;
-
-  const salt = `DECENT_GASLESS_VOTING_USER_WALLET_SALT-${EOA}-${chainId}`;
-  const saltHash = keccak256(stringToHex(salt));
-  const userSmartWalletSaltBigInt = BigInt(saltHash);
-  return userSmartWalletSaltBigInt;
-};
-
 export const getPaymasterSalt = (safeAddress: Address, chainId: number) => {
   const salt = `DECENT_GASLESS_VOTING_PAYMASTER_SALT-${safeAddress}-${chainId}`;
   const saltHash = keccak256(stringToHex(salt));
