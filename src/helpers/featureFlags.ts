a63ffcec7a4769f978fe6afcd5fff8efef1be840
twodam <twodam@protonmail.com>
2025-04-08 10:07:54 +0800

    feat(featureFlags): new flag for gasless staking

--- src/helpers/featureFlags.ts
+++ src/helpers/featureFlags.ts
@@ -1,4 +1,9 @@
-export const FEATURE_FLAGS = ['flag_dev', 'flag_gasless_voting', 'flag_iframe_template'] as const;
+export const FEATURE_FLAGS = [
+  'flag_dev',
+  'flag_gasless_voting',
+  'flag_iframe_template',
+  'flag_gasless_staking',
+] as const;
 
 export type FeatureFlagKeys = typeof FEATURE_FLAGS;
 export type FeatureFlagKey = (typeof FEATURE_FLAGS)[number];
