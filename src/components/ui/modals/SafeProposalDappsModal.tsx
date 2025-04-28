e7814a2c02f32e27413205389ba356dc1fb03584
twodam <twodam@protonmail.com>
2025-04-28 15:18:50 +0800

    feat(SafeProposalDappsModal): add title and description; add related translations

--- src/components/ui/modals/SafeProposalDappsModal.tsx
+++ src/components/ui/modals/SafeProposalDappsModal.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 import { useSupportedDapps } from '../../../hooks/DAO/loaders/useSupportedDapps';
 import { useNetworkConfigStore } from '../../../providers/NetworkConfig/useNetworkConfigStore';
 import { useDaoInfoStore } from '../../../store/daoInfo/useDaoInfoStore';
@@ -7,6 +8,7 @@ import NoDataCard from '../containers/NoDataCard';
 import { InfoBoxLoader } from '../loaders/InfoBoxLoader';
 
 export function SafeProposalDappsModal({ onClose }: { onClose: () => void }) {
+  const { t } = useTranslation(['proposalDapps']);
   const { chain } = useNetworkConfigStore();
   const { safe } = useDaoInfoStore();
   const { dapps } = useSupportedDapps(chain.id);
@@ -15,34 +17,44 @@ export function SafeProposalDappsModal({ onClose }: { onClose: () => void }) {
   const loading = !dapps || !safeAddress;
 
   return (
-    <Flex
-      flexDirection={!loading && dapps.length > 0 ? 'row' : 'column'}
-      flexWrap="wrap"
-      gap="1rem"
-    >
-      {loading ? (
-        <Box>
-          <InfoBoxLoader />
-        </Box>
-      ) : dapps.length > 0 ? (
-        dapps.map((dapp, i) => (
-          <DappCard
-            key={i}
-            title={dapp.name}
-            appUrl={dapp.url}
-            iconUrl={dapp.iconUrl}
-            description={dapp.description}
-            categories={dapp.tags}
-            safeAddress={safeAddress}
-            onClose={onClose}
+    <div>
+      <Text
+        marginTop="2rem"
+        textStyle="heading-large"
+        color="white-0"
+      >
+        {t('dappsTitle')}
+      </Text>
+      <Text color="neutral-7">{t('dappsDescription')}</Text>
+      <Flex
+        flexDirection={!loading && dapps.length > 0 ? 'row' : 'column'}
+        flexWrap="wrap"
+        gap="1rem"
+      >
+        {loading ? (
+          <Box>
+            <InfoBoxLoader />
+          </Box>
+        ) : dapps.length > 0 ? (
+          dapps.map((dapp, i) => (
+            <DappCard
+              key={i}
+              title={dapp.name}
+              appUrl={dapp.url}
+              iconUrl={dapp.iconUrl}
+              description={dapp.description}
+              categories={dapp.tags}
+              safeAddress={safeAddress}
+              onClose={onClose}
+            />
+          ))
+        ) : (
+          <NoDataCard
+            translationNameSpace="proposalDapps"
+            emptyText="emptyProposalDapps"
           />
-        ))
-      ) : (
-        <NoDataCard
-          translationNameSpace="proposalDapps"
-          emptyText="emptyProposalDapps"
-        />
-      )}
-    </Flex>
+        )}
+      </Flex>
+    </div>
   );
 }
