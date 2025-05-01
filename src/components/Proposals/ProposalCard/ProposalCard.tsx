92d3543169fc5c34d9e4df6bac8ca787d5095e45
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-05-01 01:19:47 -0400

    feat: Enhance proposal card with created by and created date display - ProposalCard.tsx: Add components to show proposal creator and creation date - proposal.json: Add translation key for created by label

--- src/components/Proposals/ProposalCard/ProposalCard.tsx
+++ src/components/Proposals/ProposalCard/ProposalCard.tsx
@@ -1,23 +1,59 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-import { format } from 'date-fns';
+import { Box, Flex, Icon as ChakraIcon, Text, Spacer } from '@chakra-ui/react';
+import { CalendarBlank } from '@phosphor-icons/react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { Address } from 'viem';
 import { DAO_ROUTES } from '../../../constants/routes';
+import { useDateTimeDisplay } from '../../../helpers/dateTime';
+import { useGetAccountName } from '../../../hooks/utils/useGetAccountName';
 import { useNetworkConfigStore } from '../../../providers/NetworkConfig/useNetworkConfigStore';
 import { useDaoInfoStore } from '../../../store/daoInfo/useDaoInfoStore';
 import { AzoriusProposal, FractalProposal, SnapshotProposal } from '../../../types';
-import { DEFAULT_DATE_FORMAT } from '../../../utils';
 import { ActivityDescription } from '../../Activity/ActivityDescription';
 import { Badge } from '../../ui/badges/Badge';
 import QuorumBadge from '../../ui/badges/QuorumBadge';
 import { SnapshotIcon } from '../../ui/badges/Snapshot';
 import { ProposalCountdown } from '../../ui/proposal/ProposalCountdown';
 
+function ProposalCreatedDate({ date }: { date: Date }) {
+  const createdDateLabel = useDateTimeDisplay(date);
+
+  return (
+    <Flex
+      gap="2"
+      alignItems="center"
+    >
+      <Text
+        textStyle="labels-small"
+        color="neutral-7"
+      >
+        {createdDateLabel}
+      </Text>
+      <ChakraIcon as={CalendarBlank} />
+    </Flex>
+  );
+}
+function ProposalCreatedBy({ createdBy }: { createdBy: Address }) {
+  const { t } = useTranslation('proposal');
+  const { displayName } = useGetAccountName(createdBy, true);
+  return (
+    <Flex
+      gap="2"
+      alignItems="center"
+    >
+      <Text
+        textStyle="labels-small"
+        color="neutral-7"
+      >
+        {t('createdBy', { createdBy: displayName })}
+      </Text>
+    </Flex>
+  );
+}
+
 function ProposalCard({ proposal }: { proposal: FractalProposal }) {
   const { safe } = useDaoInfoStore();
   const { addressPrefix } = useNetworkConfigStore();
-  const { t } = useTranslation('common');
-
   if (!safe?.address) {
     return null;
   }
@@ -65,17 +101,15 @@ function ProposalCard({ proposal }: { proposal: FractalProposal }) {
           {isAzoriusProposal && <QuorumBadge proposal={proposal as AzoriusProposal} />}
         </Flex>
         <ActivityDescription activity={proposal} />
-        <Box>
-          {proposal.eventDate && (
-            <Text
-              mt={2}
-              textStyle="labels-large"
-              color="neutral-6"
-            >
-              {t('created')} {format(proposal.eventDate, DEFAULT_DATE_FORMAT)}
-            </Text>
-          )}
-        </Box>
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          mt={4}
+        >
+          {proposal.proposer && <ProposalCreatedBy createdBy={proposal.proposer} />}
+          <Spacer />
+          {proposal.eventDate && <ProposalCreatedDate date={proposal.eventDate} />}
+        </Flex>
       </Box>
     </Link>
   );
