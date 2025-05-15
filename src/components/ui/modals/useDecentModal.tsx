85d3e33550c5f3ec1e991a738daacf81391d3eba
Kellar <kellar.base09@gmail.com>
2025-05-15 12:50:10 +0100

    Prevent duplicate modals from opening by checking existing open modals before pushing a new modal in useDecentModal hook

--- src/components/ui/modals/useDecentModal.tsx
+++ src/components/ui/modals/useDecentModal.tsx
@@ -15,10 +15,12 @@ import {
  * @returns a Function that when called opens the provided ModalType modal.
  */
 export const useDecentModal = <T extends ModalType>(modal: T, props?: ModalPropsTypes[T]) => {
-  const { pushModal } = useContext<IModalContext>(ModalContext);
+  const { pushModal, openModals } = useContext<IModalContext>(ModalContext);
   return () => {
     const modalObject = { type: modal, props: props ?? {} } as ModalTypeWithProps;
 
-    pushModal(modalObject);
+    if (openModals.findIndex(m => m.type === modal) === -1) {
+      pushModal(modalObject);
+    }
   };
 };
