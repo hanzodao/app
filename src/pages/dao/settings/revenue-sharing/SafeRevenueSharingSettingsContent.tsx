d66bf6147c87ad63872d008ae0f70269298a9785
Kellar <kellar.base09@gmail.com>
2025-06-04 13:13:49 +0100

    Add some basic UI

--- src/pages/dao/settings/revenue-sharing/SafeRevenueSharingSettingsContent.tsx
+++ src/pages/dao/settings/revenue-sharing/SafeRevenueSharingSettingsContent.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Icon, Image } from '@chakra-ui/react';
-import { Link } from '@phosphor-icons/react';
+import { Flex, Text, Icon, Image, Button } from '@chakra-ui/react';
+import { Empty, Link, Plus, TrashSimple } from '@phosphor-icons/react';
 import { useTranslation } from 'react-i18next';
 import { SettingsContentBox } from '../../../../components/SafeSettings/SettingsContentBox';
 import { BarLoader } from '../../../../components/ui/loaders/BarLoader';
@@ -22,6 +22,9 @@ export function SafeRevenueSharingSettingsPage() {
   const displayedAddress = safe?.address;
   const { displayName } = useGetAccountName(displayedAddress);
 
+  const currentDaoTreasureShare = 30;
+  const revSplitTotal = 100;
+
   return (
     <>
       {!!safe ? (
@@ -43,7 +46,7 @@ export function SafeRevenueSharingSettingsPage() {
               flexDir="column"
               p={6}
             >
-              <Text>{t('DaoSafeWallet')}</Text>
+              <Text>{t('daoSafeWallet')}</Text>
               <Flex
                 direction="row"
                 alignItems="center"
@@ -94,7 +97,71 @@ export function SafeRevenueSharingSettingsPage() {
                 <Icon as={Link} />
               </Flex>
 
-              <Divider />
+              <Divider my={4} />
+
+              <Flex
+                flexDir="row"
+                alignItems="center"
+                justifyContent="space-between"
+                gap={2}
+              >
+                <Text>{t('counterparties')}</Text>
+                <Button
+                  variant="secondary"
+                  size="sm"
+                  leftIcon={<Icon as={Plus} />}
+                >
+                  {t('revShareAddSplitWallet')}
+                </Button>
+              </Flex>
+
+              <Flex
+                flexDir="row"
+                alignItems="center"
+                mt={4}
+                border="1px solid"
+                borderColor="color-neutral-900"
+                borderRadius="0.75rem"
+                p={2}
+              >
+                <Flex
+                  flexDir="row"
+                  justifyContent="space-between"
+                  w="100%"
+                  p={3}
+                >
+                  <Text flex={4}>{t('currentDaoTreasureShareLabel')}</Text>
+                  <Text
+                    color="color-neutral-400"
+                    flex={2}
+                  >
+                    {currentDaoTreasureShare} %
+                  </Text>
+                  <Icon
+                    as={TrashSimple}
+                    color="color-lilac-100"
+                  />
+                </Flex>
+              </Flex>
+
+              <Flex
+                flexDir="row"
+                justifyContent="space-between"
+                w="100%"
+                p={6}
+              >
+                <Text
+                  color="color-neutral-500"
+                  flex={4}
+                >
+                  {t('revSplitTotalLabel')}
+                </Text>
+                <Text flex={2}>{revSplitTotal}%</Text>
+                <Icon
+                  as={Empty}
+                  color="transparent"
+                />
+              </Flex>
             </Flex>
           </Flex>
         </SettingsContentBox>
