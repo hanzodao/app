03ef56976460550a86ed39ce691e4094ef1d955a
twodam <twodam@protonmail.com>
2025-06-09 17:36:34 +0800

    feat(SafeDeployTokenPage): add standard page header and working submit button

--- src/pages/dao/deploy-token/SafeDeployTokenPage.tsx
+++ src/pages/dao/deploy-token/SafeDeployTokenPage.tsx
@@ -1,16 +1,38 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { ArrowLeft } from '@phosphor-icons/react';
 import { Formik } from 'formik';
+import { useTranslation } from 'react-i18next';
 import { initialState } from '../../../components/DaoCreator/constants';
 import { AzoriusTokenDetails } from '../../../components/DaoCreator/formComponents/AzoriusTokenDetails';
 import { DAOCreateMode } from '../../../components/DaoCreator/formComponents/EstablishEssentials';
+import PageHeader from '../../../components/ui/page/Header/PageHeader';
 import { useERC20TokenSchema } from '../../../hooks/schemas/DAOCreate/useERC20TokenSchema';
 import { CreatorFormState, CreatorSteps, GovernanceType, ICreationStepProps } from '../../../types';
 
 export function SafeDeployTokenPage() {
+  const { t } = useTranslation();
   const { erc20TokenValidation } = useERC20TokenSchema();
 
+  const pageHeaderBreadcrumbs = [
+    {
+      terminus: t('deployToken', { ns: 'breadcrumbs' }),
+      path: '',
+    },
+  ];
+
   return (
-    <Box>
+    <Box mt="2rem">
+      <PageHeader
+        title={t('tokenPageDeployTokenButton', { ns: 'settings' })}
+        breadcrumbs={pageHeaderBreadcrumbs}
+        ButtonIcon={ArrowLeft}
+        buttonProps={{
+          isDisabled: false,
+          variant: 'secondary',
+          onClick: undefined,
+        }}
+      />
+
       <Formik<Pick<CreatorFormState, 'essentials' | 'erc20Token'>>
         initialValues={{
           erc20Token: initialState.erc20Token,
@@ -21,21 +43,31 @@ export function SafeDeployTokenPage() {
           },
         }}
         validationSchema={erc20TokenValidation}
-        onSubmit={async values => {}}
+        onSubmit={async values => {
+          console.debug('DeployToken.values', values);
+        }}
         enableReinitialize
         validateOnMount
       >
         {({ handleSubmit, ...rest }) => (
-          <Box
-            as="form"
-            onSubmit={handleSubmit}
-          >
+          <form onSubmit={handleSubmit}>
             <AzoriusTokenDetails
               steps={[CreatorSteps.ERC20_DETAILS]}
               mode={DAOCreateMode.EDIT}
+              withSteps={false}
               {...(rest as any as Omit<ICreationStepProps, 'steps' | 'mode'>)}
             />
-          </Box>
+
+            <Flex
+              alignItems="center"
+              justifyContent="flex-end"
+              width="100%"
+              mt="1.5rem"
+              gap="0.75rem"
+            >
+              <Button type="submit">{t('createProposal', { ns: 'proposal' })}</Button>
+            </Flex>
+          </form>
         )}
       </Formik>
     </Box>
