5ab094f438d9c72ece2820d36b0510d23aacbb24
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-06-11 15:27:10 -0400

    refactor: update hatsTreeId to use dao-specific store slice in roles management

--- src/store/roles/useRolesStore.ts
+++ src/store/roles/useRolesStore.ts
@@ -48,17 +48,22 @@ const useRolesStore = create<RolesStore>()((set, get) => ({
     return matches[0];
   },
   setHatKeyValuePairData: args => {
-    const { hatsTreeId, contextChainId, streamIdsToHatIds } = args;
+    const { hatsTreeId, contextChainId, streamIdsToHatIds, daoKey } = args;
     for (const { hatId, streamId } of streamIdsToHatIds) {
       streamIdToHatIdMap.set(streamId, hatId);
     }
-    set(() => {
-      // if `hatsTreeId` is null or undefined,
-      // set `hatsTree` to that same value
-      if (typeof hatsTreeId !== 'number') {
-        return { hatsTreeId, hatsTree: hatsTreeId, streamsFetched: false, contextChainId: null };
+    set(state => {
+      if (hatsTreeId === null || hatsTreeId === undefined) {
+        return {
+          ...initialHatsStore,
+        };
       }
-      return { hatsTreeId, streamsFetched: false, contextChainId };
+      return {
+        ...state,
+        hatsTreeId: { [daoKey]: hatsTreeId },
+        contextChainId,
+        streamsFetched: false,
+      };
     });
   },
 
@@ -157,7 +162,6 @@ const useRolesStore = create<RolesStore>()((set, get) => ({
       hatsTreeId: state.hatsTreeId,
       contextChainId: state.contextChainId,
     })),
-  resetHatsStore: () => set(() => ({ ...initialHatsStore })),
 }));
 
 export { useRolesStore };
