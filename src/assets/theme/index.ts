a3aa7d15f7a37ced79fc409172df54b37fbc4aff
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-06-17 09:47:16 -0400

    add new box shadow

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -27,6 +27,8 @@ export const theme = mergeThemeOverride({
     useSystemColorMode: false,
   },
   shadows: {
+    'content-box-shadow':
+      '0px 0px 0px 1px rgba(248, 244, 252, 0.04), 0px 0px 0px 1px var(--colors-color-alpha-white-950), inset, 0px 2px 4px -2px var(--colors-color-alpha-black-800), 0px 4px 6px -1px var(--colors-color-alpha-black-800);',
     layeredShadowBorder:
       '0px 0px 0px 1px #100414, inset 0px 0px 0px 1px rgba(248, 244, 252, 0.04), inset 0px 1px 0px rgba(248, 244, 252, 0.04)',
   },
