16ac17354e03acd19fac39c2198ee16a3ca3933a
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-07-10 13:17:04 -0400

    refactor: consolidate subgraph endpoint and downgrade hats subgraph SDK to v1.0.0

--- src/graphql/hats.ts
+++ src/graphql/hats.ts
@@ -2,28 +2,28 @@ import { HatsSubgraphClient } from '@hatsprotocol/sdk-v1-subgraph';
 
 const theGraphAPIKey = import.meta.env.VITE_APP_THEGRAPH_API_KEY;
 const hatsSubgraphId = 'D1kbQSGSt165189Vh1CoQWjk33mSYyi5aLc3Dvb92gX7';
+const endpoint = `https://gateway.thegraph.com/api/subgraphs/id/${hatsSubgraphId}`;
 export const hatsSubgraphClient = new HatsSubgraphClient({
   config: {
     // mainnet
     [1]: {
-      endpoint: `https://gateway.thegraph.com/api/subgraphs/id/${hatsSubgraphId}`,
+      endpoint: endpoint,
       authToken: theGraphAPIKey,
     },
     // base
     [8453]: {
-      endpoint: `https://gateway.thegraph.com/api/subgraphs/id/${hatsSubgraphId}`,
+      endpoint: endpoint,
       authToken: theGraphAPIKey,
     },
     // polygon
     [137]: {
-      endpoint: `https://gateway.thegraph.com/api/subgraphs/id/${hatsSubgraphId}`,
+      endpoint: endpoint,
       authToken: theGraphAPIKey,
     },
     // optimism
     [10]: {
-      endpoint: `https://gateway.thegraph.com/api/subgraphs/id/${hatsSubgraphId}`,
+      endpoint: endpoint,
       authToken: theGraphAPIKey,
     },
-    // sepolia is handled normally.
   },
 });
